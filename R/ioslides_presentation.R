91dc975a3e78afbb77603b03ff95ee87b343d528
Mickaël Canouil <mickael.canouil@cnrs.fr>
2020-05-12 11:44:48 +0200

    Fix ressources in ioslide template

--- R/ioslides_presentation.R
+++ R/ioslides_presentation.R
@@ -17,11 +17,28 @@ ioslides_presentation <- function(
   incremental = FALSE,
   ...
 ) {
-  csl <- system.file("rmarkdown/templates/ioslides/resources/csl/apa.csl", package = "umr1283")
-  rmarkdown::ioslides_presentation(
-    css = system.file("rmarkdown/templates/ioslides/resources/mc_theme.css", package = "umr1283"),
-    logo = system.file("rmarkdown/templates/ioslides/resources/logo_UMR.png", package = "umr1283"),
-    pandoc_args = paste0("--csl=", csl),
-    ...
-  )
+  csl_default <- system.file("rmarkdown/templates/ioslides/resources/csl/apa.csl", package = "umr1283")
+  css_default <- system.file("rmarkdown/templates/ioslides/resources/mc_theme.css", package = "umr1283")
+  logo_default <- system.file("rmarkdown/templates/ioslides/resources/logo_UMR.png", package = "umr1283")
+
+  params_ellipsis <- list(...)
+  if (length(params_ellipsis) != 0) {
+    if (grepl("pandoc_args", names(params_ellipsis))) {
+      pandoc_args <- paste(
+        params_ellipsis[["pandoc_args"]],
+        paste0("--csl=", if (grepl("csl", names(params_ellipsis))) params_ellipsis[["csl"]] else csl_default)
+      )
+    } else {
+      pandoc_args <- paste0(
+        "--csl=", if (grepl("csl", names(params_ellipsis))) params_ellipsis[["csl"]] else csl_default
+      )
+    }
+    css <- if (grepl("css", names(params_ellipsis))) params_ellipsis[["css"]] else css_default
+    logo <- if (grepl("logo", names(params_ellipsis))) params_ellipsis[["logo"]] else logo_default
+  } else {
+    css <- css_default
+    logo <- logo_default
+    pandoc_args <- paste0("--csl=", csl_default)
+  }
+  rmarkdown::ioslides_presentation(css = css, logo = logo, pandoc_args = pandoc_args, ...)
 }
