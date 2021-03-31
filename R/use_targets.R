58c18d6a4e55e99d74cc8fe46cfa319e3ed11498
Mickaël Canouil <mickael.canouil@cnrs.fr>
2021-03-31 16:19:01 +0200

    functions to setup python and targets

---
+++ R/use_targets.R
@@ -0,0 +1,23 @@
+#' use_targets
+#'
+#' @param project A character string. The path a RStudio project.
+#' @param working_directory Path to the working temporary directory.
+#' @param ... Any parameter to `targets::tar_script`.
+#'
+#' @return `targets::tar_script`
+#' @export
+use_targets <- function(project = rprojroot::find_rstudio_root_file(), working_directory = "/disks/DATATMP", ...) {
+  if (nchar(system.file(package = "targets")) == 0) {
+    stop('"targets" is not installed!\nrenv::install(c("targets", "visnetwork")) to install it.')
+  }
+  dir.create(
+    path = file.path(working_directory, basename(project), "_targets"),
+    recursive = TRUE, showWarnings = FALSE, mode = "0775"
+  )
+  file.symlink(
+    from = file.path(working_directory, basename(project), "_targets"),
+    to = file.path(project, "_targets")
+  )
+  cat("_targets\n", file = file.path(project, ".gitignore"), append = TRUE)
+  targets::tar_script(...)
+}
