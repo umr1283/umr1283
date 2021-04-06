a9920a187bc24554f2a521bfb14df060b755c378
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-04-06 16:02:06 +0200

    complete docs and fix obsolete variable
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- R/use_python.R
+++ R/use_python.R
@@ -1,5 +1,7 @@
 #' use_python
 #'
+#' Setup directory structure for `python`.
+#'
 #' @param project A character string. The path a RStudio project.
 #' @param working_directory Path to the working temporary directory.
 #' @param type Type of Python environment to use.
@@ -10,14 +12,14 @@ use_python <- function(project = rprojroot::find_rstudio_root_file(), working_di
     stop('"renv" is not installed!\ninstall.packages("renv") to install it.')
   }
   dir.create(
-    path = file.path(working_directory, basename(path), "python"),
+    path = file.path(working_directory, basename(project), "python"),
     recursive = TRUE, showWarnings = FALSE, mode = "0775"
   )
   file.symlink(
-    from = file.path(working_directory, basename(path), "python"),
-    to = file.path(path, "renv", "python")
+    from = file.path(working_directory, basename(project), "python"),
+    to = file.path(project, "renv", "python")
   )
   renv::use_python(type = type)
-  system(paste(file.path(path, "renv/python/virtualenvs/renv-python-3.7.3/bin/python"), "-m ensurepip"))
+  system(paste(file.path(project, "renv/python/virtualenvs/renv-python-3.7.3/bin/python"), "-m ensurepip"))
   invisible()
 }
