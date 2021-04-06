3df8122a3d392a1c67f6c811228bd496ed4399a4
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-04-06 18:40:14 +0200

    rely on gert for git operations
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- R/use_git.R
+++ R/use_git.R
@@ -2,16 +2,17 @@
 use_git <- function(project, git_repository) {
   git_remote <- gsub("https*://(.*)/(.*)", "\\1:\\2", git_repository)
 
-  invisible(sapply(
-    X = paste("git -C", project, c(
-      "init",
-      "add --all",
-      "commit -am 'create project'",
-      "config --local core.sharedRepository 0775",
-      sprintf("push --set-upstream git@%s/%s.git master", git_remote, basename(project)),
-      sprintf("push remote add origin git@%s/%s.git master", git_remote, basename(project)),
-      "push origin master"
-    )),
-    FUN = system, ignore.stdout = TRUE, ignore.stderr = TRUE
-  ))
+  gert::git_init(project)
+  gert::git_add(files = "*", repo = project)
+  if (!gert::user_is_configured(repo = project)) {
+    stop('"user.name" and/or "user.email" are not set locally or globally. See ?gert::git_config().')
+  }
+  gert::git_commit_all(message = "create project", repo = project)
+  gert::git_config_set(name = "core.sharedRepository", value = "0775", repo = project)
+  gert::git_push(
+    remote = sprintf("git@%s/%s.git", git_remote, basename(project)),
+    set_upstream = sprintf("git@%s/%s.git", git_remote, basename(project))
+  )
+  gert::git_remote_add(url = sprintf("git@%s/%s.git", git_remote, basename(project)), repo = project)
+  invisible()
 }
