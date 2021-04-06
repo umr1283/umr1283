7b04729487e15dcf1c97a0a49f88c0817bb47914
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-04-06 15:36:03 +0200

    new function to retrieve affiliation

---
+++ R/use_affiliation.R
@@ -0,0 +1,16 @@
+#' Use UMR1283-8199 Affiliation
+#'
+#' @return A character string with the current affiliation.
+#' @export
+use_affiliation <- function() {
+  paste(
+    "Inserm U1283",
+    "CNRS UMR 8199",
+    "European Genomic Institute for Diabetes (EGID)",
+    "Institut Pasteur de Lille",
+    "University of Lille",
+    "Lille University Hospital",
+    "Lille, F-59000, France.",
+    sep = ", "
+  )
+}
