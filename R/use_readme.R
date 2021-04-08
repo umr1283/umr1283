0e5e4434d81faea7a57f0832d6f24ba995173edc
Mickaël Canouil <mickael.canouil@cnrs.fr>
2021-04-08 16:50:05 +0200

    use withr and callr

--- R/use_readme.R
+++ R/use_readme.R
@@ -1,18 +1,23 @@
 #' @keywords internal
-use_readme <- function(project = rprojroot::find_rstudio_root_file(), analyst_name) {
-  file <- file.path(project, "README.md")
-  if (!file.exists(file)) {
-    writeLines(
-      con = file,
-      text = c(
-        paste("#", basename(project)),
-        paste("Created:", Sys.time()),
-        paste("Analyst:", analyst_name),
-        paste("Affiliation:", use_affiliation())
-      ),
-      sep = "\n\n"
-    )
-  } else {
-    message(sprintf('"%s" already exists! Nothing was done!', file))
-  }
+use_readme <- function(analyst_name, project = ".") {
+  proj <- normalizePath(project, mustWork = FALSE)
+
+  withr::with_dir(proj, {
+    file <- "README.md"
+    if (!file.exists(file)) {
+      writeLines(
+        con = file,
+        text = c(
+          paste("#", basename(proj)),
+          paste("Created:", Sys.time()),
+          paste("Analyst:", analyst_name),
+          paste("Affiliation:", use_affiliation())
+        ),
+        sep = "\n\n"
+      )
+    } else {
+      message(sprintf('"%s" already exists! Nothing was done!', file))
+    }
+  })
+  invisible(TRUE)
 }
--- R/use_rproj.R
+++ R/use_rproj.R
@@ -1,28 +1,33 @@
 #' @keywords internal
 use_rproj <- function(project = ".") {
-  file <- file.path(project, ".Rproj")
-  if (!file.exists(file)) {
-    writeLines(
-      con = file,
-      text = c(
-        "Version: 1.0",
-        "",
-        "RestoreWorkspace: No",
-        "SaveWorkspace: No",
-        "AlwaysSaveHistory: No",
-        "",
-        "EnableCodeIndexing: Yes",
-        "UseSpacesForTab: Yes",
-        "NumSpacesForTab: 2",
-        "Encoding: UTF-8",
-        "",
-        "AutoAppendNewline: Yes",
-        "LineEndingConversion: Posix",
-        "",
-        "QuitChildProcessesOnExit: Yes"
+  proj <- normalizePath(project, mustWork = FALSE)
+
+  withr::with_dir(proj, {
+  file <- paste0(basename(proj), ".Rproj")
+    if (!file.exists(file)) {
+      writeLines(
+        con = file,
+        text = c(
+          "Version: 1.0",
+          "",
+          "RestoreWorkspace: No",
+          "SaveWorkspace: No",
+          "AlwaysSaveHistory: No",
+          "",
+          "EnableCodeIndexing: Yes",
+          "UseSpacesForTab: Yes",
+          "NumSpacesForTab: 2",
+          "Encoding: UTF-8",
+          "",
+          "AutoAppendNewline: Yes",
+          "LineEndingConversion: Posix",
+          "",
+          "QuitChildProcessesOnExit: Yes"
+        )
       )
-    )
-  } else {
-    message(sprintf('"%s" already exists! Nothing was done!', file))
-  }
+    } else {
+      message(sprintf('"%s" already exists! Nothing was done!', file))
+    }
+  })
+  invisible(TRUE)
 }
